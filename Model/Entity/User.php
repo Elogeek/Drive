15c4bfce706e7d4a4e30f180fbd6b6a755158f9c
elogeek <elodiechristin@gmail.com>
2021-08-12 12:55:11 +0200

    let's go

--- Model/Entity/User.php
+++ Model/Entity/User.php
@@ -1,1 +1,22 @@
 <?php
+namespace App\Entity;
+
+class User {
+
+    private ?int $id;
+    private ?string $email;
+    private ?string $password;
+
+    /**
+     * User constructor.
+     * @param int|null $id
+     * @param string|null $email
+     * @param string|null $password
+     */
+    public function __construct(int $id = null, string $email = null, string $password = null) {
+        $this->id = $id;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+}
