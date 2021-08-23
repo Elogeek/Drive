f622fcc1d845bc282d8669cf0964b317bb44e995
elogeek <elodiechristin@gmail.com>
2021-08-23 07:09:04 +0200

    let's go

---
+++ View/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My drive :)</title>
+</head>
+<body>
+<div class="container">
+    <form action="">
+        <input type="email" placeholder="Email">
+        <input type="text" placeholder="Mot de passe">
+    </form>
+</div>
+</body>
+</html>
