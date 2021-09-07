5b609c38f12ef34e0153e38b84dfb6739f8568be
elogeek <elodiechristin@gmail.com>
2021-09-07 16:24:25 +0200

    let's go

--- Model/Manager/UserManager.php
+++ Model/Manager/UserManager.php
@@ -9,12 +9,43 @@ class UserManager {
 
     /** Return a user by id */
 
-    /** Return a user with their email */
+    /** Return an User (by mail) if existing or null
+    * @param string $email
+    * @return bool
+    */
+    public function existUser(string $email): bool {
+        $request = DB::getInstance()->prepare("SELECT count(*) as cnt FROM user WHERE email = :mail");
+        $request->bindValue(':mail', $email);
+        $request->execute();
+        return intval($request->fetch()['cnt']) > 0;
+    }
 
-    /** Add user */
+    /** Add user
+     * @param User $user
+     * @return bool
+     */
+    public function addUser(User &$user): bool {
+        $request = DB::getInstance()->prepare("
+            INSERT INTO user (email,password) 
+                 VALUES(:mail,:password)
+        ");
 
-    /** Delete user */
+        $request->bindValue(":mail", $user->getEmail());
+        $request->bindValue(":password", DB::encodePassword($user->getPassword()));
+        $result = $request->execute();
+        $user->setId(DB::getInstance()->lastInsertId());
+        return $result;
+    }
 
+    /** Delete user
+     * @param User $user
+     * @return bool
+     */
+    public function deleteUser(User $user): bool {
+        $request = DB::getInstance()->prepare("DELETE FROM user WHERE id=:id");
+        $request->bindValue(":id", $user->getId());
+        return  $request->execute();
+    }
 
 }
 
