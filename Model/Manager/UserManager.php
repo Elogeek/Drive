cc3d666decde34cc116c917e1f6735869f9a6271
elogeek <elodiechristin@gmail.com>
2021-09-07 15:50:39 +0200

    let's go

--- Model/Manager/UserManager.php
+++ Model/Manager/UserManager.php
@@ -7,5 +7,21 @@ use Model\DB;
 
 class UserManager {
 
-    public function{}
-}
+    /** Return user null/not null and create user of null
+     * @param int $id
+     * @return User|null
+     */
+    public function getById(int $id): ?User {
+
+        $user = null;
+        $request = DB::getInstance()->prepare("SELECT * FROM user WHERE id= :id");
+        $request->bindValue(':id', $id);
+        $result = $request->execute();
+        $data = $request->fetch();
+
+            if($result && $data) {
+                $user = new User($data['id'], $data['email']);
+        }
+    return $user;
+
+    }
