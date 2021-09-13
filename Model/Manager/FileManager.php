376f52e221fd7fe6a0eac4f288a998a9cc6b19d5
elogeek <elodiechristin@gmail.com>
2021-09-13 15:23:47 +0200

    let's go

--- Model/Manager/FileManager.php
+++ Model/Manager/FileManager.php
@@ -1,12 +1,17 @@
 <?php
 namespace App\Model\Entity;
 
-use App\Entity\Admin;
 use App\Entity\User;
 use Model\DB;
 
 class FileManager {
 
-    /* Area Admin*/
+    public function fileById(User $user_fk): bool {
+        $request = DB::getInstance()->prepare("SELECT * FROM user WHERE user_fk= :idUser");
+        $request->bindValue("idUser", $user_fk->getId());
+        $request->execute();
+        return $request;
+    }
+
 
 }
